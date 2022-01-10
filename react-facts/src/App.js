848625b03e3da80432ad1d410eb26072a7175c0f
Oli Ullah Jewel <oli.jewel@northsouth.edu>
2022-01-11 01:09:03 +0600

    Implemented the navbar with CSS

--- react-facts/src/App.js
+++ react-facts/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
---
+++ react-facts/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../images/icons8-react-16.png";
+import "../index.css";
+
+export default function Navbar() {
+  return (
+    <div className="header-bar">
+      <div className="brand">
+        <img src={logo} alt="" />
+        <h3 className="brand-name">ReactFacts</h3>
+      </div>
+      <h3 className="text">React Course- Project 1</h3>
+    </div>
+  );
+}
